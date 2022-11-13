731956037c56c867c298db5bf7411b4e70ebb02b
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-11-13 12:40:10 +0200

    +++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+import useLocalStorage from './useLocalStorage';
+
+const LS_KEY = 'reader_item';
+
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage(LS_KEY, []);
+  const [filter, setFilter] = useState('');
+
+  const fromSubmitHandler = ({ name, number }) => {
+    const todo = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const findName = contacts.find(
+      e => e.name.toLowerCase() === todo.name.toLowerCase()
+    );
+    findName
+      ? toast.error(`${todo.name} is already in contacts`)
+      : setContacts([todo, ...contacts]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(todo =>
+      todo.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(state => state.filter(todo => todo.id !== contactId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={fromSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContacts()}
+        deleteContacts={deleteContact}
+      />
+      <ToastContainer autoClose={3000} />
+    </>
   );
-};
+}
